d5762845f087af1021b549303fba4ccfa48f033c
asif110 <asif110_pk@yahoo.com>
2018-11-03 18:14:05 +1300

    Request
    Request creation

--- MobileApp/BeamApp/BeamApp.iOS/obj/iPhoneSimulator/Debug/Request.xaml.g.cs
+++ MobileApp/BeamApp/BeamApp.iOS/obj/iPhoneSimulator/Debug/Request.xaml.g.cs
@@ -29,7 +29,16 @@ namespace BeamApp {
         private global::Xamarin.Forms.Picker cityTo;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Switch package;
+        private global::Xamarin.Forms.StackLayout urgentPanel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Switch urgent;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout sendBy;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label sendByText;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.DatePicker sendByDate;
@@ -44,17 +53,104 @@ namespace BeamApp {
         private global::Xamarin.Forms.Entry itemDescription;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry flightInformationAccompany;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout photo;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout package;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry numPackages;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout Package1Weight;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry pacakge1WeightEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Picker pacakge1WeightPicker;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout PhotoPackage1;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout Package2Weight;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry pacakge2WeightEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Picker pacakge2WeightPicker;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout PhotoPackage2;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout Package3Weight;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry pacakge3WeightEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Picker pacakge3WeightPicker;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout PhotoPackage3;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Switch firstDegree;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Switch secondDegree;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout goViral;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Switch viral;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Switch facebook;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Request));
             countryFrom = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "countryFrom");
             cityFrom = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "cityFrom");
             countryTo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "countryTo");
             cityTo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "cityTo");
-            package = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "package");
+            urgentPanel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "urgentPanel");
+            urgent = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "urgent");
+            sendBy = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "sendBy");
+            sendByText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "sendByText");
             sendByDate = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.DatePicker>(this, "sendByDate");
             flexibility = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "flexibility");
             subject = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "subject");
             itemDescription = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "itemDescription");
+            flightInformationAccompany = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "flightInformationAccompany");
+            photo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "photo");
+            package = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "package");
+            numPackages = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "numPackages");
+            Package1Weight = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "Package1Weight");
+            pacakge1WeightEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "pacakge1WeightEntry");
+            pacakge1WeightPicker = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "pacakge1WeightPicker");
+            PhotoPackage1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "PhotoPackage1");
+            Package2Weight = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "Package2Weight");
+            pacakge2WeightEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "pacakge2WeightEntry");
+            pacakge2WeightPicker = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "pacakge2WeightPicker");
+            PhotoPackage2 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "PhotoPackage2");
+            Package3Weight = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "Package3Weight");
+            pacakge3WeightEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "pacakge3WeightEntry");
+            pacakge3WeightPicker = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Picker>(this, "pacakge3WeightPicker");
+            PhotoPackage3 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "PhotoPackage3");
+            firstDegree = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "firstDegree");
+            secondDegree = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "secondDegree");
+            goViral = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "goViral");
+            viral = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "viral");
+            facebook = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Switch>(this, "facebook");
         }
     }
 }
--- MobileApp/BeamApp/BeamApp/Request.xaml.cs
+++ MobileApp/BeamApp/BeamApp/Request.xaml.cs
@@ -12,18 +12,77 @@ namespace BeamApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Request : ContentPage
 	{
+        public enum RequestType
+        {
+            REQUEST_TYPE_INVALID = 0,
+            REQUEST_TYPE_DOCUMENT = 1,
+            REQUEST_TYPE_PACKAGE,
+            REQUEST_TYPE_ACCOMPANY,
+            REQUEST_CARPOOL
+        }
+
         List<CityController.CountryName> m_countryList;
         CityController m_cityController = new CityController();
         int m_iCityFrom = 0;
         int m_iCityTo = 0;
+        RequestType m_requestType = RequestType.REQUEST_TYPE_INVALID;
+       
 
-        public Request ()
+        public Request (RequestType requestType)
 		{
 			InitializeComponent ();
             m_countryList = CityController.GetCountries().ToList();
             countryFrom.ItemsSource = m_countryList.ToList();
             countryTo.ItemsSource = m_countryList.ToList();
+            Title = "Request";
 
+            switch (requestType)
+            {
+                case RequestType.REQUEST_TYPE_DOCUMENT:
+                    ShowDocument();
+                    break;
+                case RequestType.REQUEST_TYPE_PACKAGE:
+                    ShowPackage();
+                    break;
+                case RequestType.REQUEST_TYPE_ACCOMPANY:
+                    ShowAccompany();
+                    break;
+                case RequestType.REQUEST_CARPOOL:
+                    ShowCarpool();
+                    break;
+            }
+            m_requestType = requestType;
+        }
+
+        private void ShowDocument()
+        {
+            flightInformationAccompany.IsVisible = false;
+            package.IsVisible = false;
+        }
+        private void ShowPackage()
+        {
+            flightInformationAccompany.IsVisible = false;
+            photo.IsVisible = false;
+        }
+
+        private void ShowAccompany()
+        {
+            package.IsVisible = false;
+            goViral.IsVisible = false;
+            urgentPanel.IsVisible = false;
+            flexibility.IsVisible = false;
+            sendByText.Text = "Departure Date";
+            photo.IsVisible = false;
+        }
+
+        private void ShowCarpool()
+        {
+            flightInformationAccompany.IsVisible = false;
+            package.IsVisible = false;
+            urgentPanel.IsVisible = false;
+            sendBy.IsVisible = false;
+            flightInformationAccompany.IsVisible = false;
+            flexibility.IsVisible = false;
         }
 
         async void OnCountryFromSelected(object sender, EventArgs e)
@@ -53,6 +112,34 @@ namespace BeamApp
             {
                 m_iCityTo = city.PK;
             }
+        }
+
+        async void OnCreateRequest(object sender, EventArgs e)
+        {
+            RequestController requestController = new RequestController();
+            int options = 0;
+            if (firstDegree.IsToggled) options = 1;
+            if (secondDegree.IsToggled) options = 2;
+            if (viral.IsToggled) options = 3;
+
+            int flexibilityDays = 0;
+            int.TryParse(flexibility.Text, out flexibilityDays);
+
+            bool result = await requestController.CreateRequest(m_requestType, m_iCityFrom, m_iCityTo, sendByDate.Date, urgent.IsToggled,
+                                                                flexibilityDays, subject.Text, itemDescription.Text, "TBI", options,
+                                                                facebook.IsToggled);
+
+            if (result)
+            {
+                await DisplayAlert("Be a sender", "Your request is created ", "OK");
+                await Navigation.PopAsync();
+
+            }
+            else
+            {
+                await DisplayAlert("Be a sender", "Error occured, please try again later", "OK");
+            }
+
         }
     }
 }
